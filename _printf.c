1ddfb3b453037efb881aded01748625d5999107c
tsehayget <tsehayget4@gmail.com>
2022-07-12 09:04:30 -0700

    printf function

--- _printf.c
+++ _printf.c
@@ -2,9 +2,7 @@
 /**
 * _printf - printf function
 * @format: const character pointer
-* 
 * Return: returns number of characters
-* 
 */
 int _printf(const char *format, ...)
 {
@@ -26,16 +24,16 @@ int _printf(const char *format, ...)
 * loop - loop format
 * @l: va_list arg
 * @string: pointer from format
-* 
 * Return: num of characters
 */
 int loop(va_list l, const char *string)
 {
 	int i = 0, f = 0, count_f = 0, count = 0, checker = 0;
+
 	while (i < _strlen((char *)string) && *string != '\0')
-	
 	{
 		char c = string[i];
+
 		if (c == '%')
 		{
 			i++, f++;
@@ -47,7 +45,7 @@ int loop(va_list l, const char *string)
 			if (c == '%')
 			{
 				f++;
-			} 
+			}
 			else
 			{
 				count_f = function_man(c, l);
@@ -56,21 +54,51 @@ int loop(va_list l, const char *string)
 					f++;
 					c = string[i];
 					if (c == '%')
-					f--;
-					count = count + count_f;
+					f--, count = count + count_f;
 				}
-		
 				else if (count_f == -1 && c != '\n')
-				{		
+				{
 					count += _putchar('%');
 				}
-			}		
-		}	
+			}
+		}
 		checker = check_percent(&f, c);
 		count += checker;
 		if (checker == 0 && c != '\0' && c != '%')
-		count += _putchar(c), i++;
-		checker = 0;
+		count += _putchar(c), i++, checker = 0;
 	}
+	return (count);
+}
+/**
+ * check_percent - call function manager
+ * @f: value by reference
+ * @c: character
+ * Return: 1 if % is printed
+ */
+int check_percent(int *f, char c)
+{
+	int tmp_f;
+	int count = 0;
+
+	tmp_f = *f;
+	if (tmp_f == 2 && c == '%')
+	{
+		_putchar('%');
+		tmp_f = 0;
+		count = 1;
+	}
+	return (count);
+}
+/**
+ * call_function - call function man
+ * @c: character parameter
+ * @l: list of arguments
+ * Return: num of characters
+ */
+int call_function(char c, va_list l)
+{
+	int count = 0;
+
+	count = function_man(c, l);
 	return (count);
 }
