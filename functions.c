ac65f118a3b30238e8dead526ab4cc079da44038
Agemasie Amare <agemasie@gmail.com>
2022-07-12 19:21:37 +0000

    functions  edited

--- functions.c
+++ functions.c
@@ -2,19 +2,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * _putchar - writes the character to output
- * @c: the character to print
- * Return: 1 on success and on error returns -1
- */
+*
+* _putchar - writes the character to output
+* @c: the character to print
+* Return: 1 on success
+* on error returns -1
+*
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 /**
- * _puts - prints string
- * @str: pointer char
- * Return: nothing
- */
+* _puts - prints string
+* @str: pointer char
+*
+* Return: nothing
+*
+*/
+
+
 void _puts(char *str)
 {
 	int i;
