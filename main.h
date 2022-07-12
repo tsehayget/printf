948328ff5325017b9df6a9b827d52bd7d54201eb
Agemasie Amare <agemasie@gmail.com>
2022-07-12 15:48:29 +0000

    heading

--- main.h
+++ main.h
@@ -13,6 +13,16 @@ int loop(va_list l, const char *string);
 int check_percent(int *f, char c);
 int call_function(char c, va_list l);
 int _switch(char c, va_list l);
+int print_character(va_list l);
+int print_string(va_list l);
+int print_sign(va_list l, int b);
+int print_unsign(va_list l, int b);
+int print_base16(va_list l, char *rep);
+int print_rev(va_list l);
+int print_rot13(va_list l);
+int print_ptr(va_list l);
+char *convert_to(char rep[], unsigned int n, int b);
+int print_ptr(va_list l);
 
 
 #endif
